3f4f6c4a8aa262749c254339990a0e41bd4ae26e
Lachlan Jowett <lochyjowett1@gmail.com>
2022-07-17 20:52:38 +1000

    Adding source control

---
+++ World/Grass.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func create_grass_effect():
+	var GrassEffect = load("res://Effects/GrassEffect.tscn")
+	var grassEffect = GrassEffect.instance()
+	var main = get_tree().current_scene
+	main.add_child(grassEffect)
+	grassEffect.global_position = global_position
+
+func _on_HurtBox_area_entered(area):
+	create_grass_effect()
+	queue_free()
