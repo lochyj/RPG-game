c0fb0b4c46f2c223d444b7f510d16604a8787218
Lachlan Jowett <lochyjowett1@gmail.com>
2022-07-23 14:53:41 +1000

    Did some organisation to make my life not hell

--- Player/PlayerStats.gd
+++ Player/PlayerStats.gd
@@ -1,14 +1,27 @@
 extends Node
 
-export(int) var maxHealth = 1
-onready var health = maxHealth setget setHealth
+export(int) var maxHealth = 1 setget setMaxHealth
+var health = maxHealth setget setHealth
 
 signal no_health
 signal health_changed(value)
+signal max_health_changed(value)
 # signal no_stamina
+
+func setMaxHealth(value):
+	maxHealth = value
+	if health != null:
+		self.health = min(health, maxHealth)
+	else:
+		self.health = maxHealth
+	emit_signal("max_health_changed", maxHealth)
 
 func setHealth(value):
 	health = value
 	emit_signal("health_changed", health)
+	# Death / no health signal
 	if health <= 0:
 		emit_signal("no_health")
+
+func _ready():
+	self.health = maxHealth
--- Player/UI.gd
+++ Player/UI.gd
@@ -1,19 +1,44 @@
 extends Control
 
+
+# ---------------
+# Health UI admin
+# ---------------
 var hearts = 5 setget setHearts
 var maxHearts = 5 setget setMaxHearts
 
-onready var label = $Label
+# Full hearts UI element
+onready var heartUIFull = $HeartUIFull
+# Empty hearts UI element
+onready var heartUIEmpty = $HeartUIEmpty
+
+
+# ---------------
+# Health UI funcs
+# ---------------
 
 func setHearts(value):
 	hearts = clamp(value, 0, maxHearts)
-	if label != null:
-		label.text = str(hearts)+ "/" + str(maxHearts)
+	if heartUIFull != null:
+		heartUIFull.rect_size.x = hearts * 15
 
 func setMaxHearts(value):
 	maxHearts = max(value, 1)
+	self.hearts = min(hearts, maxHearts)
+	if heartUIEmpty != null:
+		heartUIEmpty.rect_size.x = maxHearts * 15
+
+
+
+# ----------
+# Init stuff
+# ----------
 
 func _ready():
+	# ---------
+	# Health UI
+	# ---------
 	self.maxHearts = PlayerStats.maxHealth
 	self.hearts = PlayerStats.health
 	PlayerStats.connect("health_changed", self, "setHearts")
+	PlayerStats.connect("max_health_changed", self, "setMaxHearts")
